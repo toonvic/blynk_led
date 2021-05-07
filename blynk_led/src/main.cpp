98d73076b1ea174892f53cf107bf81ca284a1509
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-05-07 09:28:15 -0300

    - Adição de variáveis para identificação das portas dos leds;

--- blynk_led/src/main.cpp
+++ blynk_led/src/main.cpp
@@ -1,5 +1,9 @@
 #define BLYNK_PRINT Serial
 
+#define LED_PIN_D1 5
+#define LED_PIN_D2 4
+#define LED_PIN_D3 0
+
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
@@ -10,9 +14,9 @@ char pass[] = "password";
 
 void setup()
 {
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(0, OUTPUT);
+  pinMode(LED_PIN_D1, OUTPUT);
+  pinMode(LED_PIN_D2, OUTPUT);
+  pinMode(LED_PIN_D3, OUTPUT);
 
   Serial.begin(9600);
 
