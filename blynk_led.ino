60acabae683e51132d31f9597d0d9ff7a30b7318
Victor do Amaral <60518597+toonvic@users.noreply.github.com>
2021-05-11 11:30:13 -0300

    Adição do .ino

---
+++ blynk_led.ino
@@ -0,0 +1,28 @@
+#define BLYNK_PRINT Serial
+
+#define LED_PIN_D1 5
+#define LED_PIN_D2 4
+#define LED_PIN_D3 0
+
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "my-auth";
+char ssid[] = "my-wifi";
+char pass[] = "my-wifi-password";
+
+void setup()
+{
+  //Definição dos pinos
+  pinMode(LED_PIN_D1, OUTPUT);
+  pinMode(LED_PIN_D2, OUTPUT);
+  pinMode(LED_PIN_D3, OUTPUT);
+
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
